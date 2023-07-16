5848973736ad5a3923c2509077a7c8813ce5637e
shaffy95 <onozareshaffy95@gmail.com>
2023-07-16 14:30:37 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - determine if a number is positive, negative or zero.
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int n;
+	char ch;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch >= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+	return (0);
+}
