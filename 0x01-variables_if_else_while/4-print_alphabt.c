b86a32683bdb9db92c98c54c120b281b0c55e83d
shaffy95 <onozareshaffy95@gmail.com>
2023-07-16 11:03:44 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,22 +5,16 @@
 */
 int main(void)
 {
-	char c;
+	char c = 'a';
 
-	char d;
-
-	c = 'a';
-	d = 'A';
-	while
-		(c <= 'z') {
-			putchar(c);
-			c++;
-		}
-	while
-		(d <= 'Z') {
-			putchar(d);
-			d++;
-		}
+	while (c <= 'z')
+	{
+		if
+			(c != 'e' && c != 'q') {
+				putchar(c);
+			}
+		c++;
+	}
 	putchar('\n');
 	return (0);
 }
