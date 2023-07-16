d313244dcc48b91357926eed468322620f7b126e
shaffy95 <onozareshaffy95@gmail.com>
2023-07-16 11:34:36 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - determine if a number is positive, negative or zero.
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int ch = 0;
+
+	while (ch < 10)
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	putchar('\n');
+	return (0);
+}
