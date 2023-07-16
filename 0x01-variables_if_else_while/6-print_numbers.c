1bb7cf4851e889107c16906b63577704c6927969
shaffy95 <onozareshaffy95@gmail.com>
2023-07-16 12:34:28 -0700

    6-print_numbers

---
+++ 0x01-variables_if_else_while/6-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - determine if a number is positive, negative or zero.
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int n = 0;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
