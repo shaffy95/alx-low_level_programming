b573961893af6c72684c96e8060f98d29458ba95
shaffy95 <onozareshaffy95@gmail.com>
2023-07-19 06:38:38 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,12 +12,20 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			k = j * i;
-			_putchar(k + '0');
-			if (j != 9)
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+			else
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
+				if (k < 10)
+				{
+					_putchar(' ');
+				}
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 		}
 		_putchar('\n');
