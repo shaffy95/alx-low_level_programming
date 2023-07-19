083318d9c29b59fd28f1b87baa99c29ce6c6aca6
shaffy95 <onozareshaffy95@gmail.com>
2023-07-19 07:34:58 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * function that adds two integers and returns the result
+ * Return - Always 0 (success)
+ * check code
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
