9f8623b8191e96367b8dc20fd7ae54e7134f71de
shaffy95 <onozareshaffy95@gmail.com>
2023-08-11 10:56:20 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,20 +5,20 @@
  * is_positive_number - Check if a string is a positive number
  * @str: The input string
  *
- * Returns: 1 if the string is a positive number, 0 otherwise
+ * Returns: pointer to new allocated memory
  */
 int is_positive_number(const char *str)
 {
 	if (str == NULL || *str == '\0')
-		return 0;
+		return (0);
 
 	while (*str)
 	{
 		if (*str < '0' || *str > '9')
-			return 0;
+			return (0);
 		str++;
 	}
-	return 1;
+	return (1);
 }
 
 /**
@@ -26,22 +26,22 @@ int is_positive_number(const char *str)
  * @argc: The number of command-line arguments
  * @argv: An array containing the command-line arguments
  *
- * Returns: 0 on success, 1 otherwise
+ * Returns: 0 on success 98 on faliure
  */
 int main(int argc, char *argv[])
 {
-	unsigned long num1, num2;
+	unsigned long long num1, num2;
 
 	if (argc != 3 || !is_positive_number(argv[1]) || !is_positive_number(argv[2]))
 	{
 		printf("Error\n");
-		return 98;
+		return (98);
 	}
 
-	num1 = strtoul(argv[1], NULL, 10);
-	num2 = strtoul(argv[2], NULL, 10);
+	num1 = strtoull(argv[1], NULL, 10);
+	num2 = strtoull(argv[2], NULL, 10);
 
-	printf("%lu\n", num1 * num2);
+	printf("%llu\n", num1 * num2);
 
-	return 0;
+	return (0);
 }
