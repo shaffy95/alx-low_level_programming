d5206663f0da6875eac166f3ae3a55c7cb852b28
shaffy95 <onozareshaffy95@gmail.com>
2023-08-14 16:03:28 +0100

    2-main.c

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program that prints the name of the file it was compiled from
+ * followed by a new line
+ *
+ * Return: 0 Always success
+ */
+
+int main(void)
+{
+	printf("02-main.c\n");
+	return (0);
+}
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program that prints the name of the file it was compiled from
+ * followed by a new line
+ *
+ * Return: 0 Always success
+ */
+
+int main(void)
+{
+	printf("2-main.c\n");
+	return (0);
+}
