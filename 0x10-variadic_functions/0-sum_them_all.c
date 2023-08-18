5c93ba3f5da6131475fbd15613e616ff35c47aba
shaffy95 <onozareshaffy95@gmail.com>
2023-08-18 14:30:11 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,24 @@
 #include <stdarg.h>
-#include <stdio.h>
+
+/**
+ * sum_them_all - Calculates the Sum of Input Values.
+ *
+ * @n: Number of Input Variables
+ *
+ * Return: The Calculated Sum of the Variables
+ */
+
 
 int sum_them_all(const unsigned int n, ...)
+{
+	va_list pi;
+	unsigned int pet, sum = 0;
+
+	va_start(pi, n);
+
+	for (pet = 0; pet < n; pet++)
+		sum += va_arg(pi, int);
+	va_end(pi);
+
+	return (sum);
+}
