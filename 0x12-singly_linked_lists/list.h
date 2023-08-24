927658df42da3f184a0567b3f1e92782a720eba1
shaffy95 <onozareshaffy95@gmail.com>
2023-08-24 16:57:57 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/list.h
@@ -0,0 +1,30 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * struct list_s - singly linked list
+ * list_len - Counts the number of elements in a linked list.
+ * @h: Pointer to the head of the linked list.
+ * Return: Number of elements (nodes) in the list.
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+size_t list_len(const list_t *h);
+size_t print_list(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif /* LIST_H */
