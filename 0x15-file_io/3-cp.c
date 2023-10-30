c834e598d6c84a8531d6723760e8d063b5d49333
shaffy95 <onozareshaffy95@gmail.com>
2023-10-30 08:18:54 -0700

    file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,53 +8,42 @@
 #define WRITE_ERR "Error: Can't write to %s\n"
 
 /**
- * main - Entry point for checking code of Holberton School students.
- *
- * @argc: Number of arguments.
- * @argv: Array of argument strings.
- *
+ * main - check the code for Holberton School students.
+ * @argc: num of args
+ * @argv: args
  * Return: Always 0.
  */
 int main(int argc, char **argv)
 {
-	int dur, pi, top_up, m, n;
+	int rom, dest, top_up, m, n;
 	char buffer[1024];
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	pi = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (pi == -1)
+		dprintf(STDERR_FILENO, "Usage: cp file_rom file_dest\n"), exit(97);
+	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (dest == -1)
 		dprintf(STDERR_FILENO, WRITE_ERR, argv[2]), exit(99);
-	dur = open(argv[1], O_RDONLY);
-	if (dur == -1)
+	rom = open(argv[1], O_RDONLY);
+	if (rom == -1)
 		dprintf(STDERR_FILENO, READ_ERR, argv[1]), exit(98);
 	while (1)
 	{
-		n = read(dur, buffer, 1024);
+		n = read(rom, buffer, 1024);
 		if (n == -1)
 			dprintf(STDERR_FILENO, READ_ERR, argv[1]), exit(98);
 		if (n > 0)
 		{
-			m = write(pi, buffer, n);
+			m = write(dest, buffer, n);
 			if (m == -1)
 				dprintf(STDERR_FILENO, WRITE_ERR, argv[2]), exit(99);
 		} else
 			break;
 	}
-	top_up = close(dur);
+	top_up = close(rom);
 	if (top_up == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dur), exit(100);
-	top_up = close(pi);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", rom), exit(100);
+	top_up = close(dest);
 	if (top_up == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dur), exit(100);
-	top_up = close(pi);
-	if (top_up == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dur), exit(100);
-	top_up = close(pi);
-	if (top_up == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dur), exit(100);
-	top_up = close(pi);
-	if (top_up == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", pi), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest), exit(100);
 	return (0);
 }
